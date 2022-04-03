fe6de7cd769a1c72fc591b88d244bd917e09d5e3
James Remus <James.Remus@veteransunited.com>
2022-04-03 14:42:07 -0500

    created list to print in order

--- displayInventory.py
+++ displayInventory.py
@@ -1,7 +1,16 @@
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 inventory = dict()
+
 def displayInventory(inventory):
-    print (inventory)
+    index = 0
+    print ("Inventory:")
+    for item in inventory:
+        items = list(inventory.keys())
+        quantityItems = list(inventory.values())
+        print (quantityItems[index], items[index])
+        index = index + 1
+    
+
         
 
 
